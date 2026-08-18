a2377a34ee6f8a6e1e3e8674075ea70315154f3e
Boris <boris@de-neve.nl>
2026-08-18 23:06:26 +0200

    Let Lars and Laura seal an alliance by pairing a Jedi with a princess.

--- js/app.js
+++ js/app.js
@@ -542,7 +542,7 @@ let openingPromise = null;
 function ensureOpening() {
   if (openingPromise) return openingPromise;
   const track = document.querySelector("[data-crawl-track]");
-  openingPromise = fetch("data/opening.html?v=4")
+  openingPromise = fetch("data/opening.html?v=6")
     .then((res) => {
       if (!res.ok) throw new Error("opening");
       return res.text();
